efbd8bf3de090381275fe6480d0a0f9ba1c2f3ca
Kaio.r.rodrigues <110853592+Rodegheri22@users.noreply.github.com>
2022-12-07 15:31:37 -0300

    Add files via upload

---
+++ PRII-2/variaveis.js
@@ -0,0 +1,15 @@
+var x = prompt("Digite seu nome:");
+x= String(x);
+
+var y = prompt("Digite sua idade:");
+y = parseInt(y);
+
+var nome = x 
+document.write("  Nome do usuario: "    , x ,)
+
+var idade = y
+document.write("  tem a idade:"     , y,)
+
+
+
+
